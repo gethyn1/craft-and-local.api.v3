da7b64aa5a5356e0dbe7970054d646c26e7ce471
Gethyn Jones <gethyn1@gmail.com>
2019-05-03 17:53:17 +0100

    Refactor app initialisation to return server instance

--- src/web/server/connect-database.js
+++ src/web/server/connect-database.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-const onConnected = (onConnect) => () => {
-  console.log('Mongo DB connected')
-  onConnect()
-}
-
-const onError = err => console.error(err)
+const onConnected = () => console.log('Mongo DB connected')
 
 const onDisconnected = () => console.log('Mongo DB disconnected')
+
+const onError = err => console.error(err)
 
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
@@ -17,17 +14,24 @@ process.on('SIGINT', () => {
   })
 })
 
-const connectDatabase = (onConnect, config, app) => {
-  mongoose.connect(config.environment.MONGODB_URI, { useNewUrlParser: true })
+const connectDatabase = async (config, app) => {
+  // Fix deprecation notice
   mongoose.set('useCreateIndex', true)
   // Fix deprecation notice. Should be removed when updating to Mongoose v6
   // https://github.com/Automattic/mongoose/issues/6880#issuecomment-435621543
   mongoose.set('useFindAndModify', false)
-  mongoose.connection.on('connected', onConnected(onConnect))
+  mongoose.connection.on('connected', onConnected)
   mongoose.connection.on('error', onError)
   mongoose.connection.on('disconnected', onDisconnected)
+
+  await mongoose.connect(config.environment.MONGODB_URI, { useNewUrlParser: true })
+}
+
+const disconnectDatabase = () => {
+  mongoose.disconnect()
 }
 
 module.exports = {
-  connectDatabase
+  connectDatabase,
+  disconnectDatabase
 }
--- src/web/server/server.js
+++ src/web/server/server.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser')
 const helmet = require('helmet')
 
 const { setHeaders } = require('./set-headers')
-const { connectDatabase } = require('./connect-database')
+const { connectDatabase, disconnectDatabase } = require('./connect-database')
 const { registerRoutes } = require('../routes')
 const { registerErrorHandlers } = require('./error-handlers')
 
@@ -12,7 +12,7 @@ const listen = (config, app) =>
   )
 
 // TO DO: sanitise all incoming data
-const start = (config, app) => () => {
+const start = (config, app) => {
   app.use(helmet())
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,12 +20,15 @@ const start = (config, app) => () => {
   setHeaders(config, app)
   registerRoutes(config, app)
   registerErrorHandlers(app)
-  listen(config, app)
+
+  return listen(config, app)
 }
 
-const initialise = (config, app) => {
-  const startApp = start(config, app)
-  connectDatabase(startApp, config, app)
+const initialise = async (config, app) => {
+  await connectDatabase(config, app)
+  const server = start(config, app)
+  server.once('close', disconnectDatabase)
+  return server
 }
 
 module.exports = {
