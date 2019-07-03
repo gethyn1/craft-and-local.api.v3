afefbe461da8322eb0d519c5c6dcae6303cb6888
Gethyn Jones <gethyn1@gmail.com>
2019-07-03 20:10:22 +0100

    Remove filters from find producers

--- src/web/services/mongodb/producers/find.js
+++ src/web/services/mongodb/producers/find.js
@@ -1,29 +1,9 @@
-const { compose } = require('ramda')
 const { Producer } = require('./model')
 
-const {
-  excludeFilter,
-  latlngFilter,
-  mindistanceFilter,
-  setFilter
-} = require('./filters')
-
-const setCategoryFilter = setFilter('categories')
-const setExcludeFilter = setFilter('exclude', excludeFilter)
-const setLatlngFilter = setFilter('latlng', latlngFilter)
-const setMindistanceFilter = setFilter('mindistance', mindistanceFilter)
-
-const find = async ({ categories, exclude, latlng, limit, mindistance }) => {
+const find = async ({ limit }) => {
   try {
-    const filters = compose(
-      setMindistanceFilter(mindistance),
-      setLatlngFilter(latlng),
-      setExcludeFilter(exclude),
-      setCategoryFilter(categories)
-    )({})
-
     const results = await Producer
-      .find(filters)
+      .find()
       .limit(parseInt(limit, 10))
       .exec()
 
