f8598ad57933718eb701d30ecc39fef363f61d79
Gethyn Jones <gethyn1@gmail.com>
2019-08-29 20:00:17 +0100

    Add type predicates

--- src/predicates/types.js
+++ src/predicates/types.js
@@ -3,3 +3,9 @@ const { compose, equals, type } = require('ramda')
 module.exports.isString = compose(equals('String'), type)
 
 module.exports.isFunction = compose(equals('Function'), type)
+
+module.exports.isObject = compose(equals('Object'), type)
+
+module.exports.isNumber = compose(equals('Number'), type)
+
+module.exports.isArray = compose(equals('Array'), type)
